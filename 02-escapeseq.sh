9cd004bc8846bb41cc89af6e107a482463e4cd3c
unknown <samsung150973@gmail.com>
2023-03-09 13:11:51 +0000

    change

--- 02-escapeseq.sh
+++ 02-escapeseq.sh
@@ -2,6 +2,7 @@
 echo line1
 echo line2
 echo line2
-echo to enable escape sequnce use -e and ""
+echo to enable escape sequnce use -e  with options /t /n
 echo -e "LineX\tLineY"
 echo -e "LineA\nLineB"
+echi -e "LINE-a\n\tLINE-b"
