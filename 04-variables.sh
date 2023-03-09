eb0c32ee1e77a323e9319650af9e6bbe7f87f13e
unknown <samsung150973@gmail.com>
2023-03-09 18:13:56 +0000

    change

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,6 +6,10 @@ b=20
 c=30
 
 echo to print a variable
-echo $a
-echo ${b}
-echo "$c"
+echo "I am printing the vlaue of $a"
+echo "I am printing the vlaue of ${b}"
+echo "I am printing the value of $c"
+
+echo if a variable is not declared , i will print empty
+
+echo " I am printing value of d which is not declared $d"
