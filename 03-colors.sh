8c384e14a5829810137557e76f65ce3e9a394392
unknown <samsung150973@gmail.com>
2023-03-09 15:13:29 +0000

    change

---
+++ 03-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# For a colored background
+# reset = 0, black = 40, red = 41, green = 42, yellow = 43, blue = 44, magenta = 45, cyan = 46, and white=47, are the commonly used color codes.
+
+# For foreground
+# reset = 0, black = 30, red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36, and white = 37.
+
+echo -e "\e[32m This is in Green Colour \e [0m"
