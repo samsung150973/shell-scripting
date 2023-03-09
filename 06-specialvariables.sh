74b44fe022ee89954ef5417481f244e6d20f47a2
unknown <samsung150973@gmail.com>
2023-03-09 18:49:28 +0000

    change

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -1,4 +1,6 @@
 
 # special Characters are: $0 to $9 , $* ,#@, $$
 # $0 gives the current script name in execution
-echo = "Name of the current script is : $0"
+echo  "Name of the current script is : $0"
+echo  "Name of the current script is : $1"
+echo  "Name of the current script is : $2"
