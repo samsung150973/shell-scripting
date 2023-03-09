94479b4b10081bc3405bb6a3ee88267ff9b7b73f
unknown <samsung150973@gmail.com>
2023-03-09 18:30:06 +0000

    change

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -2,5 +2,10 @@
 
 TODAYDATE="09/03/2023"
 echo "today date is ${TODAYDAYE}"
+
 #To print in colours
 echo -e "today date is \e[32m ${TODAYDATE} \e[0m"
+
+# The date is static. To get current date the command is 
+
+sysdate="Today date is ${date +%F}"
