da9c96b623ca649383be32605b795f73d78f34b1
unknown <samsung150973@gmail.com>
2023-03-09 15:18:00 +0000

    change

--- 03-colors.sh
+++ 03-colors.sh
@@ -6,4 +6,10 @@
 # For foreground
 # reset = 0, black = 30, red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36, and white = 37.
 
-echo -e "\e[32m This is in Green Colour \e [0m"
+echo -e "\e[31m This is in red Colour \e[0m"
+echo -e "\e[32m This is in Green Colour \e[0m"
+echo -e "\e[33m This is in yellow Colour \e[0m"
+echo -e "\e[34m This is in blue Colour \e[0m"
+echo -e "\e[35m This is in magenta Colour \e[0m"
+echo -e "\e[36m This is in Cyan Colour \e[0m"
+echo -e "\e[37m This is in White Colour \e[0m"
