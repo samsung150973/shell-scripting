ade65733c927a58e3d51ac2c1bc1c7ddf3cf328e
unknown <samsung150973@gmail.com>
2023-03-10 12:07:47 +0000

    change

--- 07-read.sh
+++ 07-read.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # take user input and print it
-read -p "enter the user running the script :"NAME
+read -p "enter the user running the script : "NAME
 # value entered is assigned to variable NAME
 echo "user running the script is $NAME"
