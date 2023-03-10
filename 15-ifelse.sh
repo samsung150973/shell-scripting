3d1ab69d78492b28aa583a2475096b22a587ed77
unknown <samsung150973@gmail.com>
2023-03-10 22:29:57 +0000

    change

---
+++ 15-ifelse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+<<COMMENT
+if command used in 3 ways
+    1) simple If
+    2) If-Else
+    3) Else-If
+
+    if [ expression ] ; then
+        command 1
+    else
+        command2
+    fi
+COMMENT
