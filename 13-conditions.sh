aac5d1dd9946cbdf6a2676fcf78993b9a683707e
unknown <samsung150973@gmail.com>
2023-03-10 21:59:51 +0000

    change

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # syntax for case
 
+ACTION=$1 # gives the value of 1st argumemnt supplied on command line
 case $ACTION in
     start)
         echo "starting payment service"
@@ -11,6 +12,8 @@ case $ACTION in
     restart
         echo "restart the payment service"
         ;;
-
-
+    *)
+        echo -e "valid options are \e[32m Start or Stop or restart \g[0m
+        ;;
 esac
+
