fd487e114d6706b650522d0665abf6b8df34ac41
unknown <samsung150973@gmail.com>
2023-03-10 12:27:19 +0000

    change

--- 07-read.sh
+++ 07-read.sh
@@ -3,3 +3,4 @@
 read -p "enter the user running the script :" NAME
 # value entered is assigned to variable NAME
 echo "user running the script is $NAME"
+# above is good to break sequence of execution run from the command prompt 
---
+++ 08-functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# binary  command           (called from /bin or /sbin e.g command -- type cat --  will show the dir name of cat process)
+# aliaises command          (are shortcuts -- alias gp="get pull")
+# shell built-in command    (e.g command -- type cd --   or ---type export --)
+# functions                 ( set of comamnds in a seq - to be called-in when required)
+
+# Declaring a function
+
+sample() {
+    echo " I am a function
+}
