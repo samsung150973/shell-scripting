e477047454712582ccd0ffcc159312bfad21f6c6
unknown <samsung150973@gmail.com>
2023-03-10 22:32:33 +0000

    change

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -11,4 +11,18 @@ if command used in 3 ways
     else
         command2
     fi
+
+    Another Example is
+        if [ expression ] ; then
+        command 1
+    elif
+        [ expression ] ; then
+        command2
+    
+    elif
+        [ expression ] ; then
+        command3
+    else
+        command 4
+    fi
 COMMENT
