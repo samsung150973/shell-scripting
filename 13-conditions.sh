dc485b686ba679c7f190d54fb06a6059d5b2b91b
unknown <samsung150973@gmail.com>
2023-03-10 22:01:38 +0000

    change

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -13,7 +13,6 @@ case $ACTION in
         echo "restart the payment service"
         ;;
     *)
-        echo -e "valid options are \e[32m Start or Stop or restart \g[0m
+        echo -e "valid options are \e[32m Start or Stop or restart \g[0m"
         ;;
 esac
-
