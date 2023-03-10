02a12d229d249755b0dfc043ffb94b2caa7c2ade
unknown <samsung150973@gmail.com>
2023-03-10 12:55:23 +0000

    change

--- 08-functions.sh
+++ 08-functions.sh
@@ -10,6 +10,12 @@
 
 sample() {
     echo " I am a function"
+    echo " Today date is $(date +%F)"
+    echo " system uptime is $(uptime)"
+    echo " awk -F is a function used with field seperator"
+    echo " first value of uptime command output is  $(uptime | awk -F : '{print $01}')"
+    echo " last field of the uptime command output is $(uptime | awk -F : '{print $NF}')"
+    echo " load average from uptime is $(uptime | awk -F : '{print $NF}' | awk =F , `{print $1}`)"
 }
 sample
 sample
