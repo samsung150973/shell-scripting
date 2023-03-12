9d5eede2d45465824f27f90c8cec4ca0916de9d8
unknown <samsung150973@gmail.com>
2023-03-12 18:27:59 +0000

    change

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -71,7 +71,7 @@ systemctl enable nginx &>> /tmp/frontend.log
 systemctl restart nginx &>> /tmp/frontend.log
 
 # restart Nginx
-restart nginx
+systemctl restart nginx
 
 if [ $? -eq 0 ] ; then
     echo -e " \e[32m success \e[0m"
