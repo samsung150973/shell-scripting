3971d3f92a4674cf2b652f7e35dbe323bde666df
unknown <samsung150973@gmail.com>
2023-03-14 23:58:38 +0000

    change

--- robot/components/cart.sh
+++ robot/components/cart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "user automation script"
+echo "cart automation script"
 
 # set -e # exist the prog if any error ( disabled with # as comment for this prog)
-COMPONENT=user # to remove repetion of the name frontend. and also helps not to hardcode the filename
+COMPONENT=cart # to remove repetion of the name frontend. and also helps not to hardcode the filename
 source components/common.sh # calls the functions file
 NODEJS
 
