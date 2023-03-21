4831be989c1a4f0c03e575d025fa6685e7570747
unknown <samsung150973@gmail.com>
2023-03-21 22:32:16 +0000

    change

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -37,7 +37,7 @@ IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
 # search with value component and replace with $COMPONENT and search for IPaddress & change the ipaddress; the value from the file record.json
 echo $IPADDRESS
 sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/$IPADDRESS/" robot/record.json > /tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file://tmp/record.json | jq
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:/tmp/record.json | jq
 
 
 
